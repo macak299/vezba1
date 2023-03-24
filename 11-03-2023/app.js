05c5fbc1c84777c56158ac23f1089e941f0f796e
DESKTOP-BP0JN6V\Toma <macak299@gmail.com>
2023-03-24 19:39:28 +0100

    11.03.2023.

---
+++ 11-03-2023/app.js
@@ -0,0 +1,23 @@
+render() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+        <li>Item 6</li>
+      </ul>
+      <ol>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ol>
+      <img src="https://via.placeholder.com/150" alt="placeholder" />
+    </div>
+  );
+}
